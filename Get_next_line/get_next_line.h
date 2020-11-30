c2c44cbc16d0a291cd66f7cc238df774d8461c9a
Tanguy BILLON <tanguy_billon@hotmail.fr>
2020-11-30 07:42:47 -0800

    GNL - init

---
+++ Get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbillon <tbillon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/30 05:40:59 by tbillon           #+#    #+#             */
+/*   Updated: 2020/11/30 07:42:11 by tbillon          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+int		get_next_line(int fd, char **line);
+
+#endif
