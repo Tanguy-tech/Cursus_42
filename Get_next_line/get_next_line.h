1a7d883d412381e3139ccfed0f10e4a9306ab6a6
Tanguy BILLON <tanguy_billon@hotmail.fr>
2020-12-03 13:50:05 +0100

    *.h

--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tbillon <tbillon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 05:40:59 by tbillon           #+#    #+#             */
-/*   Updated: 2020/12/02 16:47:37 by tbillon          ###   ########lyon.fr   */
+/*   Updated: 2020/12/03 13:49:09 by tbillon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ int		ft_strlen(char *s);
 char	*save_next(char *s, char c);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(char *s, unsigned int start, size_t len);
+int		get_line(char **line, char **tmp, char **next, int val);
 
 #endif
