487bce6a884a1e0b0ab79a7939187b34a0d4d269
Tanguy Billon <55744941+Tanguy-tech@users.noreply.github.com>
2020-12-21 14:22:51 +0100

    Add files via upload

---
+++ Print_f/ft_converter_utils.c
@@ -0,0 +1,229 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_converter_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbillon <tbillon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/10 08:46:12 by tbillon           #+#    #+#             */
+/*   Updated: 2020/12/21 13:01:45 by tbillon          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	str_pad_width(t_Printf *print_f, char *str)
+{
+	if (print_f->type == 'p')
+	{
+		str = ft_strjoin_p("0x", str);
+		if (print_f->flags == 2)
+			write_precision_str(print_f, str);
+		if (print_f->width >= 0)
+		{
+			write_width_str(print_f, str);
+			if (print_f->flags != 2)
+				write_precision_str(print_f, str);
+		}
+	}
+	else
+	{
+		if (print_f->flags == 2)
+			write_precision_str(print_f, str);
+		if (print_f->width >= 0)
+		{
+			write_width_str(print_f, str);
+			if (print_f->flags != 2)
+				write_precision_str(print_f, str);
+		}
+	}
+}
+
+void	num_pad_width(t_Printf *print_f, long i)
+{
+	int		neg;
+	char	*str;
+	int		j;
+	char	*it;
+
+	neg = 0;
+	if (print_f->precision > print_f->width)
+		j = print_f->precision;
+	else
+		j = print_f->width;
+	str = calloc(sizeof(char), j + 1);
+	if (i < 0)
+	{
+		if (i != -2147483648)
+		{
+			i = ft_abs(i);
+			neg = 1;
+			str = ft_strjoin_c('-', str);
+		}
+	}
+	it = ft_itoa(i);
+	if ((j = print_f->precision) > 0 || (print_f->flags == 1 && print_f->precision == 0))
+	{
+		if (print_f->flags == 1 && print_f->precision <= 0)
+		{
+			if (neg == 1)
+				j = print_f->width - 1;
+			else
+				j = print_f->width;
+		}
+		while (j - ft_strlen(it) > 0)
+		{
+			str = ft_strjoin_c('0', str);
+			j--;
+		}
+		str = ft_strjoin(str, it);
+	}
+	else
+	{
+		if (print_f->precision < 0 && i == 0)
+		{
+			if (print_f->width > 0)
+				str = ft_strjoin(str, " ");
+			else
+				str = ft_strjoin(str, NULL);
+		}
+		else
+			str = ft_strjoin(str, it);
+	}
+	if ((j = print_f->width) > 0 && print_f->width > print_f->precision)
+	{
+		if (print_f->precision > 0 && print_f->precision > ft_strlen(it))
+			j -= print_f->precision;
+		else
+			j -= ft_strlen(it);
+		if (neg == 1)
+			j -= 1;
+		if (print_f->flags == 2)
+		{
+			if (str == NULL)
+			{
+				str = "\0";
+				j += 1;
+			}
+			while (j > 0)
+			{
+				str = ft_strjoin_c(' ', str);
+				j--;
+			}
+		}
+		else if (print_f->flags != 1 || (print_f->flags == 1 && print_f->width > print_f->precision && print_f->precision > 0))
+		{
+			while (j > 0)
+			{
+				str = ft_strjoin_left(' ', str);
+				j--;
+			}
+		}
+		else if (print_f->flags == 1 && print_f->precision < 0 && print_f->width > print_f->precision)
+		{
+			if (str == NULL)
+			{
+				str = "\0";
+				j += 1;
+			}
+			while (j > 0)
+			{
+				str = ft_strjoin_left(' ', str);
+				j--;
+			}
+		}
+	}
+	print_f->result += ft_putstr(str, ft_strlen(str));
+	free(str);
+	free(it);
+}
+
+void	hexa_pad_width(t_Printf *print_f, char *hexa)
+{
+	char	*str;
+	int		j;
+
+	if (print_f->type == 'x' || print_f->type == 'X')
+	{
+		if (print_f->precision > print_f->width)
+			j = print_f->precision;
+		else
+			j = print_f->width;
+		str = calloc(sizeof(char), j + 1);
+		if ((j = print_f->precision) > 0 || (print_f->flags == 1 && print_f->precision == 0))
+		{
+			if (print_f->flags == 1 && print_f->precision <= 0)
+				j = print_f->width;
+			while (j - ft_strlen(hexa) > 0)
+			{
+				str = ft_strjoin_left('0', str);
+				j--;
+			}
+			str = ft_strjoin(str, hexa);
+		}
+		else
+		{
+			if (print_f->precision < 0 && ft_strcmp(hexa, "0") == 0)
+			{
+				if (print_f->width > 0)
+					str = ft_strjoin(str, " ");
+				else
+					str = ft_strjoin(str, NULL);
+			}
+			else
+				str = ft_strjoin(str, hexa);
+		}
+		if ((j = print_f->width) > 0 && print_f->width > print_f->precision)
+		{
+			if (print_f->precision > 0 && print_f->precision > ft_strlen(hexa))
+				j -= print_f->precision;
+			else
+				j -= ft_strlen(hexa);
+			if (print_f->flags == 2)
+			{
+				if (str == NULL)
+				{
+					str = "\0";
+					j += 1;
+				}
+				while (j > 0)
+				{
+					str = ft_strjoin_c(' ', str);
+					j--;
+				}
+			}
+			else if (print_f->flags != 1 || (print_f->flags == 1 && print_f->width > print_f->precision && print_f->precision > 0))
+			{
+				while (j > 0)
+				{
+					str = ft_strjoin_left(' ', str);
+					j--;
+				}
+			}
+			else if (print_f->flags == 1 && print_f->precision < 0 && print_f->width > print_f->precision)
+			{
+				if (str == NULL)
+				{
+					str = "\0";
+					j += 1;
+				}
+				while (j > 0)
+				{
+					str = ft_strjoin_left(' ', str);
+					j--;
+				}
+			}
+		}
+		print_f->result += ft_putstr(str, ft_strlen(str));
+		free(str);
+	}
+}
+
+void	star_condition(t_Printf *print_f)
+{
+	if (print_f->width < 0)
+	{
+		print_f->width = ft_abs(print_f->width);
+		print_f->flags = 2;
+	}
+}
---
+++ Print_f/ft_printf.h
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbillon <tbillon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/04 13:24:36 by tbillon           #+#    #+#             */
+/*   Updated: 2020/12/18 13:12:50 by tbillon          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "libftprintf/libft.h"
+# include <stdio.h>
+
+typedef struct	s_Printf
+{
+	char type; /* cspduixX% */
+	int	 flags; /* Flags "0->1" "- ->2" "+ ->3"*/
+	int	 width; /* number * => sets the minimum field width for an output value */
+	int	 precision; /* .number .* => max number of caracter to print (or minimum number fo interger digits) */
+	int  star;
+	int	 result; /* Will be the result of the printf function (integer that represent the lenght of the printed chars) */
+}				t_Printf;
+
+int				ft_printf(const char *format, ...);
+t_Printf		*initialize_struct(void);
+t_Printf		*reset_struct(t_Printf *struct_name);
+
+int				regular_types(char c);
+int				parse_format(const char *format, t_Printf *print_f, va_list args);
+
+/*--- Arguments controller ---*/
+void			define_convert_args(t_Printf *print_f, va_list args, char type);
+void			convert_cs(t_Printf *print_f, va_list args);
+void			convert_num(t_Printf *print_f, va_list args);
+void			convert_pxx(t_Printf *print_f, va_list args);
+void			convert_percent(t_Printf *print_f);
+
+/*----- Width controller -----*/
+int				add_width(t_Printf *print_f, va_list args, const char *format);
+int				star_flag(t_Printf *print_f, va_list args, const char *format);
+void			star_condition(t_Printf *print_f);
+void			spaces_type_width(t_Printf *print_f, int len);
+void			zero_type_width(t_Printf *print_f, int len);
+void			write_width_c(t_Printf *print_f);
+void			write_width_str(t_Printf *print_f, char *str);
+void			write_width_num(t_Printf *print_f, int num);
+
+/*---- Justify controller ---*/
+int				justify(t_Printf *print_f, const char *pad);
+void			str_pad_width(t_Printf *print_f, char *str);
+void			num_pad_width(t_Printf *print_f, long i);
+void			hexa_pad_width(t_Printf *print_f, char *hexa);
+
+/*--- Precision controller --*/
+int				set_precision(t_Printf *print_f,  va_list args, const char *prec);
+void			write_precision_str(t_Printf *print_f, char *str);
+
+#endif
